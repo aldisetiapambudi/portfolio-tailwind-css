fb1c19c0be1e078eeac9adac3d72d7c45f82da71
aldisetiapambudi <aldisetiapambudi@gmail.com>
2022-04-11 06:56:16 +0700

    penambahan komponen, font, icofont, dan perubahan beberapa section sesuai dengan kebutuhan

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,25 @@ module.exports = {
         primary : '#14b8a6',
         dark: '#0f172a',
         secondary: '#64748b',
-        kas: '#ff6b00'
+        kas: '#ff6b00',
+        kedua: '#8391FD',
       },
       screens: {
         '2xl': '1320px',
       },
       fontFamily: {
         inter: ['Inter']
+      },
+      animation:{
+        'bounce-slow' : 'bounce 6s linear infinite',
+        'upDown' : 'updown 2s ease-in-out infinite',
+      },
+      keyframes: {
+        updown: {
+            '0%, 100%': { transform: 'translateY(-10px)'},
+            '50%': { transform: 'translateY(0px)'},
+          },
+                
       },
     },
   },
