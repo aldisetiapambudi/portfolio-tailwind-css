a896469e74d6d9d364d639a6f183d5873c94c8b2
aldisetiapambudi <aldisetiapambudi@gmail.com>
2022-04-12 14:24:44 +0700

    belum tambah apa apa :-(

--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-// dark mode
-const checkbox = document.querySelector('#toggle');
-const html = document.querySelector('html');
-checkbox.addEventListener('click', function() {
-    checkbox.checked ? html.classList.add('dark') : html.classList.remove('dark')
-})
-
-
